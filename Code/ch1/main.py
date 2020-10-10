c32d7a4b202cc4a2b28bdd6203c377110e97e004
cc <1183864793@qq.com>
2020-10-10 22:07:48 +0800

    cc

--- Code/ch1/main.py
+++ Code/ch1/main.py
@@ -4,6 +4,16 @@ from agent import RandomAgent, RLAgent, HumanPlayer
 from MyEnvs.tic_tac_toe import TicTacToe
 
 
+def self_play_train(env, player1, player2):
+    env.reset()
+    train_epochs = 3
+    for epoch in range(train_epochs):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("epoch:", epoch)
+        env.render()
+        time.sleep(1)
+
+
 def human_vs_agent():
     human_player = HumanPlayer(env=env)
 
